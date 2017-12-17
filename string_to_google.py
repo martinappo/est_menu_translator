e57dfadab68bd04e34b104cbf1d58716464ee652
renno <renno.raudmae@gmail.com>
2017-12-17 15:16:52 +0200

    image path made

--- string_to_google.py
+++ string_to_google.py
@@ -1,6 +1,9 @@
 import urllib.request as request
 import urllib
 import re
+import os
+
+IMAGES = 'images'
 
 try:
     import Image
@@ -21,16 +24,25 @@ def search_google_image(search_term):
                                      'searchType': "image", 'key': 'AIzaSyCHiNZOR3jks2sguztp3xnLoH1plzURkow', 'cx' : '012763076801589293049:rxhgz0ebbac'})
     data = response.json()
     try:
-        img_link = data['items'][0]['link']
-        print(img_link)
-        img_name = search_term.strip().replace(" ", "")
-        img_name = re.sub('[^A-Za-z0-9]+', '', img_name)[:10]
-        urllib.request.urlretrieve(img_link, "{}.jpg".format(img_name))
-        return (img_link)
+        return save_image(search_term, data)
 
     except KeyError:
         print('No image found for {}'.format(search_term))
-        return search_google_image('no image found')
+        return ('no_photo.jpg')
+
+def save_image(search_term, data):
+
+    img_link = data['items'][0]['link']
+    img_name = search_term.strip().replace(" ", "")
+    img_name = re.sub('[^A-Za-z0-9]+', '', img_name)[:10]
+
+    if not os.path.exists(IMAGES):
+        os.makedirs(IMAGES)
+
+    img_path = "{}/{}.jpg".format(IMAGES, img_name)
+    urllib.request.urlretrieve(img_link, img_path)
+
+    return img_path
 
 
 
